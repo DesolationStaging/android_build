93af094cd0719157176adb69ca53d171dec02a8f
Snuzzo <jrracinfan@gmail.com>
2015-02-24 04:50:03 -0500

    Add .md5 cleaning

--- core/cleanbuild.mk
+++ core/cleanbuild.mk
@@ -206,6 +206,7 @@ installclean_files := \
 	$(PRODUCT_OUT)/*.xlb \
 	$(PRODUCT_OUT)/*.zip \
 	$(PRODUCT_OUT)/*.zip.md5sum \
+	$(PRODUCT_OUT)/*.zip.md5 \
 	$(PRODUCT_OUT)/kernel \
 	$(PRODUCT_OUT)/data \
 	$(PRODUCT_OUT)/skin \
